9e229fecddd9a03c927b15ac1a73a7ad1dbe55c8
yousef <yousefsameh24@gmail.com>
2023-05-30 19:36:23 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,39 +1,9 @@
 import _ from 'lodash';
-import myName from './myName.js';
-import './style.css';
-import Icon from './icon.jpeg';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json5 from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json5.title); // output `JSON5 Example`
-console.log(json5.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, currently included via a script, is required for this line to work
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.innerHTML += myName('Sha3bola');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    console.log(Notes);
 
     return element;
   }
